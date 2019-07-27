76e08e32f688feabd8a4cd443ac4f863479fe4ad
Anthony Dickson <eight0153@gmail.com>
2019-07-27 23:01:37 +0000

    add unzip to system dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo Installing system dependencies...
-sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
+sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev unzip
 
 echo Installing Miniconda...
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
