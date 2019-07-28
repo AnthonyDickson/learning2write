0e18aaaebe89c34c2643d68ab2d9a4accda0c561
Anthony Dickson <eight0153@gmail.com>
2019-07-28 21:31:57 +0000

    update experiment utility script

--- run_experiments.sh
+++ run_experiments.sh
@@ -28,12 +28,14 @@ done
 shift $((OPTIND - 1))
 
 x_server_num=0
+mkdir -p logs
 
 function start_run() {
-    x_server_num=${x_server_num} + 1
+    x_server_num=$((x_server_num+1))
 
-    nohup xvfb-run -s "-screen 0 1200x800x24" -n ${x_server_num}  python train.py -model-type $1 -policy-type $2 \
-        -pattern-set $3 -updates ${n_steps} &> nohup.$1_$3.out &
+    nohup xvfb-run -e /dev/stdout -s "-screen 0 1200x800x24" -n ${x_server_num} \
+ 	python train.py -model-type $1 -policy-type $2 -pattern-set $3 -updates ${n_steps} \
+	&> logs/nohup.$1_$2_$3.out &
 
 }
 
@@ -43,13 +45,13 @@ start_run ppo mlp 5x5
 start_run acktr mlp digits
 start_run ppo mlp digits
 
-start_run acktr cnn digits
-start_run ppo cnn digits
+start_run acktr mlp emnist
+start_run ppo mlp emnist
 
 start_run acktr cnn emnist
 start_run ppo cnn emnist
 
 
-nohup tensorboard --logdir tensorboard/ &> nohup.tensorboard.out &
+nohup tensorboard --logdir tensorboard/ &> logs/nohup.tensorboard.out &
 
 
