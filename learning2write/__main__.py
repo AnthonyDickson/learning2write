ca92d6a7acf17482c31349e38066586f64a546d9
Anthony Dickson <eight0153@gmail.com>
2019-07-28 22:44:06 +1200

    tidy up text logging for testing scripts

--- learning2write/__main__.py
+++ learning2write/__main__.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from statistics import mean
 
 import plac
 
@@ -15,24 +16,35 @@ def main(pattern_set='3x3', max_steps=100, fps=4):
     """Run a demo of a random agent in the learning2write environment."""
     with WritingEnvironment(get_pattern_set(pattern_set)) as env:
         episode = 0
+        steps = 0
+        rewards = []
 
         while True:
             episode += 1
+            episode_rewards = []
             env.reset()
 
             for step in range(max_steps):
                 start = datetime.now()
                 action = env.action_space.sample()
-                _, _, done, _ = env.step(action)
+                _, reward, done, _ = env.step(action)
+                episode_rewards.append(reward)
 
                 env.render()
                 env.wait(1.0 / fps - (datetime.now() - start).total_seconds())  # sync steps to framerate (`fps`)
+                print('\rEpisode %02d - Step %02d - Reward: %.2f - Cumulative Reward: %.2f - Mean Reward: %.2f'
+                      % (episode, step + 1, reward, sum(episode_rewards), mean(episode_rewards)), end='')
+                steps += 1
 
                 if done or env.should_quit:
                     break
 
             if env.should_quit:
                 break
+
+            rewards.append(sum(episode_rewards))
+            print('\rEpisode %02d - Steps: %d - Episode Reward: %.2f - Smoothed Avg. Reward: %.2f'
+                  % (episode, steps, reward, mean(rewards[-1 - min(len(rewards) - 1, 100):])) + ' ' * 40)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from statistics import mean
 
 import plac
-from stable_baselines import ACKTR
+from stable_baselines.common import ActorCriticRLModel
 
 from learning2write import get_pattern_set
 from learning2write.env import WritingEnvironment
@@ -10,14 +10,14 @@ from learning2write.env import WritingEnvironment
 
 @plac.annotations(
     model_path=plac.Annotation('The path and the filename of the saved model to run.',
-                               type=str, kind='option'),
+                               type=str, kind='positional'),
     pattern_set=plac.Annotation('The set of patterns to use in the environment.', choices=['3x3', '5x5'],
                                 kind='option', type=str),
     max_updates=plac.Annotation('The maximum number of steps to perform in the evironment.', type=int, kind='option'),
     max_steps=plac.Annotation('The maximum number of steps to perform per episode.', type=int, kind='option'),
     fps=plac.Annotation('How many steps to perform per second.', type=float, kind='option')
 )
-def main(model_path='acktr_learning2write', pattern_set='3x3', max_updates=1000, max_steps=100, fps=2.0):
+def main(model_path, pattern_set='3x3', max_updates=1000, max_steps=100, fps=2.0):
     """Run a model in the writing environment in test mode (i.e. no training, just predictions).
 
     Press `Q` or `ESCAPE` to quit at any time.
@@ -26,7 +26,7 @@ def main(model_path='acktr_learning2write', pattern_set='3x3', max_updates=1000,
     pattern_set = get_pattern_set(pattern_set)
 
     # TODO: Make type of model configurable via cli
-    model = ACKTR.load(model_path)
+    model = ActorCriticRLModel.load(model_path)
 
     with WritingEnvironment(pattern_set) as env:
         episode = 0
@@ -38,8 +38,8 @@ def main(model_path='acktr_learning2write', pattern_set='3x3', max_updates=1000,
             steps, reward = run_episode(env, episode, fps, updates, max_updates, max_steps, model)
             rewards.append(reward)
             updates += steps
-            print('\rEpisode %02d - Steps: %d - Reward: %d - Mean Reward: %.2f'
-                  % (episode, steps, reward, mean(rewards[min(len(rewards) - 1, 100):])))
+            print('\nEpisode %02d - Steps: %d - Episode Reward: %.2f - Smoothed Avg. Reward: %.2f'
+                  % (episode, steps, reward, mean(rewards[-1 - min(len(rewards) - 1, 100):])) + ' ' * 40)
 
             if env.should_quit:
                 break
@@ -47,16 +47,17 @@ def main(model_path='acktr_learning2write', pattern_set='3x3', max_updates=1000,
 
 def run_episode(env, episode, fps, updates, max_updates, max_steps, model):
     observation = env.reset()
-    episode_reward = 0
     step = 0
+    rewards = []
 
     for step in range(max_steps):
         start = datetime.now()
         action, _ = model.predict(observation)
         observation, reward, done, _ = env.step(action)
-        episode_reward += reward
+        rewards.append(reward)
 
-        print('\rEpisode %02d - Step %02d - Reward: %d' % (episode, step + 1, reward), end='')
+        print('\rEpisode %02d - Step %02d - Reward: %.2f - Cumulative Reward: %.2f - Mean Reward: %.2f'
+              % (episode, step + 1, reward, sum(rewards), mean(rewards)), end='')
         env.render()
         env.wait(1.0 / fps - (datetime.now() - start).total_seconds())  # sync steps to framerate (`fps`)
         updates += 1
@@ -64,7 +65,7 @@ def run_episode(env, episode, fps, updates, max_updates, max_steps, model):
         if done or env.should_quit or updates >= max_updates:
             break
 
-    return step + 1, episode_reward
+    return step + 1, sum(rewards)
 
 
 if __name__ == '__main__':
