7b4509b80ebea1a761bbba2ae1ec4624f0fe196c
Anthony Dickson <eight0153@gmail.com>
2019-07-28 12:43:56 +1200

    add policy type into tensorboard log name; clean up and document training code

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 import os
 from datetime import datetime
+from typing import Optional, Type, Tuple
 
 import numpy as np
 import plac as plac
 import tensorflow as tf
-from stable_baselines import ACKTR
+from stable_baselines import ACKTR, PPO2
 from stable_baselines.a2c.utils import conv, conv_to_fc, linear
-from stable_baselines.common import BaseRLModel
-from stable_baselines.common.policies import MlpPolicy, CnnPolicy
+from stable_baselines.common import ActorCriticRLModel
+from stable_baselines.common.policies import FeedForwardPolicy, MlpPolicy, CnnPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 
 from learning2write import WritingEnvironment, get_pattern_set, EMNIST_PATTERN_SETS, VALID_PATTERN_SETS
@@ -42,7 +43,7 @@ class CheckpointHandler:
 
         return True
 
-    def save_model(self, model: BaseRLModel, checkpoint_name=None):
+    def save_model(self, model: ActorCriticRLModel, checkpoint_name=None):
         """Save a checkpoint.
 
         :param model: The model to save.
@@ -57,7 +58,7 @@ class CheckpointHandler:
 
 def emnist_cnn_feature_extractor(scaled_images, **kwargs):
     """
-    CNN from Nature paper.
+    CNN feature extractor for EMNIST images (28x28).
 
     :param scaled_images: (TensorFlow Tensor) Image input placeholder
     :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
@@ -70,44 +71,16 @@ def emnist_cnn_feature_extractor(scaled_images, **kwargs):
     return activ(linear(layer_3, 'fc1', n_hidden=256, init_scale=np.sqrt(2)))
 
 
-@plac.annotations(
-    model_path=plac.Annotation('Continue training a model specified by a path to a saved model.',
-                               type=str, kind='option'),
-    policy_type=plac.Annotation('The type of policy network to use.Ignored if loading a model.',
-                                type=str, kind='option', choices=['mlp', 'cnn']),
-    pattern_set=plac.Annotation('The set of patterns to use in the environment.', choices=VALID_PATTERN_SETS,
-                                kind='option', type=str),
-    updates=plac.Annotation('How steps to train the model for.', type=int, kind='option'),
-    checkpoint_path=plac.Annotation('The directory to save checkpoint data to. '
-                                    'Defaults to \'checkpoints/<pattern-set>/\'', type=str, kind='option'),
-    checkpoint_frequency=plac.Annotation('How often (in number of updates, not timesteps) to save the model during '
-                                         'training. Set to zero to disable checkpointing.', type=int, kind='option'),
-    n_workers=plac.Annotation('How many workers, or cpus, to train with.', type=int, kind='option')
-)
-def main(model_path=None, policy_type='mlp', pattern_set='3x3', updates=100000,
-         checkpoint_path=None, checkpoint_frequency=1000,
-         n_workers=4):
-    """Train an ACKTR RL agent on the learning2write environment."""
+def get_checkpointer(checkpoint_frequency: int, checkpoint_path: Optional[str], model: ActorCriticRLModel,
+                     pattern_set: str) -> Optional[CheckpointHandler]:
+    """Create a CheckpointHandler based on certain parameters.
 
-    env = SubprocVecEnv([lambda: WritingEnvironment(get_pattern_set(pattern_set)) for _ in range(n_workers)])
-    tensorboard_log = "./tensorboard/"
-
-    # TODO: Make type of model configurable via cli
-    if model_path:
-        model = ACKTR.load(model_path, tensorboard_log=tensorboard_log)
-        model.set_env(env)
-    else:
-        if policy_type == 'mlp':
-            policy = MlpPolicy
-        elif policy_type == 'cnn':
-            assert pattern_set in EMNIST_PATTERN_SETS, 'A CNN policy must be used with an EMNIST pattern set.'
-            policy = CnnPolicy
-        else:
-            raise 'Unrecognised policy type \'%s\'' % policy_type
-
-        model = ACKTR(policy, env, verbose=1, tensorboard_log=tensorboard_log,
-                      policy_kwargs={'cnn_extractor': emnist_cnn_feature_extractor})
-
+    :param checkpoint_frequency: How often to save checkpoints. Checkpoints are disabled if this is less than one.
+    :param checkpoint_path: Where to save the checkpoints. If `None` then a path is automatically generated.
+    :param model: The model to save training progress for.
+    :param pattern_set: The name of the set of patterns that the model will be trained on.
+    :return: A CheckpointHandler if `checkpoint_checkpoint_frequency` > 0, None otherwise.
+    """
     if checkpoint_frequency > 0:
         timestamp = ''.join(map(lambda s: '%02d' % s, datetime.now().utctimetuple()))
         path = checkpoint_path if checkpoint_path else 'checkpoints/%s_%s_%s/' % (model.__class__.__name__.lower(),
@@ -116,10 +89,90 @@ def main(model_path=None, policy_type='mlp', pattern_set='3x3', updates=100000,
         checkpointer = CheckpointHandler(checkpoint_frequency, path)
     else:
         checkpointer = None
+    return checkpointer
+
+
+def get_model_type(model_type) -> Type[ActorCriticRLModel]:
+    """Translate a model name from a string to a class type.
+
+    :param model_type: The name of the type of model.
+    :return: The class corresponding to the name.
+             Raises ValueError if the name is not recognised.
+    """
+    if model_type == 'acktr':
+        return ACKTR
+    if model_type == 'ppo':
+        return PPO2
+    else:
+        raise ValueError('Unrecognised model type \'%s\'' % model_type)
+
+
+def get_policy(policy_type: str, pattern_set: str) -> Tuple[Type[FeedForwardPolicy], dict]:
+    """Translate a policy type from a string to a class type.
+
+    :param policy_type: The name of the type of policy.
+    :param pattern_set: The name of the pattern set that the model will be trained on.
+    :return: The class corresponding to the name and the relevant kwargs dictionary.
+             Raises ValueError if the name is not recognised.
+    """
+    if policy_type == 'mlp':
+        policy = MlpPolicy
+        policy_kwargs = dict()
+    elif policy_type == 'cnn':
+        assert pattern_set in EMNIST_PATTERN_SETS, 'A CNN policy must be used with an EMNIST pattern set.'
+        policy = CnnPolicy
+        policy_kwargs = {'cnn_extractor': emnist_cnn_feature_extractor}
+    else:
+        raise 'Unrecognised policy type \'%s\'' % policy_type
+    return policy, policy_kwargs
+
+
+@plac.annotations(
+    model_path=plac.Annotation('Continue training a model specified by a path to a saved model.',
+                               type=str, kind='option'),
+    model_type=plac.Annotation('The type of model to use. This is ignored if loading a model.',
+                               choices=['acktr', 'ppo'],
+                               type=str, kind='option'),
+    policy_type=plac.Annotation('The type of policy network to use. This is ignored if loading a model.',
+                                choices=['mlp', 'cnn'],
+                                type=str, kind='option'),
+    pattern_set=plac.Annotation('The set of patterns to use in the environment.',
+                                choices=VALID_PATTERN_SETS,
+                                kind='option', type=str),
+    updates=plac.Annotation('How steps to train the model for.',
+                            type=int, kind='option'),
+    checkpoint_path=plac.Annotation('The directory to save checkpoint data to. '
+                                    'Defaults to \'checkpoints/<pattern-set>/\'',
+                                    type=str, kind='option'),
+    checkpoint_frequency=plac.Annotation('How often (in number of updates, not timesteps) to save the model during '
+                                         'training. Set to zero to disable checkpointing.',
+                                         type=int, kind='option'),
+    n_workers=plac.Annotation('How many workers, or cpus, to train with.',
+                              type=int, kind='option')
+)
+def main(model_path=None, model_type='acktr', policy_type='mlp', pattern_set='3x3', updates=100000,
+         checkpoint_path=None, checkpoint_frequency=1000,
+         n_workers=4):
+    """Train an ACKTR RL agent on the learning2write environment."""
+
+    env = SubprocVecEnv(
+        [lambda: WritingEnvironment(get_pattern_set(pattern_set, batch_size=1028)) for _ in range(n_workers)])
+    tensorboard_log = "./tensorboard/"
+
+    if model_path:
+        model = ActorCriticRLModel.load(model_path, tensorboard_log=tensorboard_log)
+        model.set_env(env)
+    else:
+        policy, policy_kwargs = get_policy(policy_type, pattern_set)
+        model = get_model_type(model_type)(policy, env, verbose=1, tensorboard_log=tensorboard_log,
+                                           policy_kwargs=policy_kwargs)
+
+    checkpointer = get_checkpointer(checkpoint_frequency, checkpoint_path, model, pattern_set)
 
     try:
-        model.learn(total_timesteps=updates, tb_log_name='ACKTR_%s_%s' % (pattern_set.upper(),
-                                                                          model.policy.__name__.upper()),
+        model.learn(total_timesteps=updates, tb_log_name='%s_%s_%s' % (pattern_set.upper(),
+                                                                       model.__class__.__name__.upper(),
+                                                                       model.policy.__name__.upper()),
                     reset_num_timesteps=model_path is None, callback=checkpointer)
         checkpointer.save(model, "checkpoint_last" % pattern_set)
     except KeyboardInterrupt:
